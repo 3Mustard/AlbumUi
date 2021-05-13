ce3028d4d6fc7479895ac2776bd92af052b61c81
Matt <m23.cullen@gmail.com>
2021-05-12 20:02:04 -0400

    add new imports formsmodule and reactiveformsmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,6 @@ import { AlbumComponent } from './album/album.component';
 import { ShowAlbmComponent } from './album/show-albm/show-albm.component';
 import { AddEditAlbmComponent } from './album/add-edit-albm/add-edit-albm.component';
 import { SharedService } from './shared.service';
-
 
 
 @NgModule({
@@ -25,7 +26,10 @@ import { SharedService } from './shared.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [SharedService],
   bootstrap: [AppComponent]
